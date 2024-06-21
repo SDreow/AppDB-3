cf6b23a6c80cc56f072a6f1fbc218ce922c72bab
Štěpán Velínský <stteranv@gmail.com>
2024-06-21 15:10:57 +0200

    Customers - Validation

--- AppDB-3/Model/Customers.cs
+++ AppDB-3/Model/Customers.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using DevExpress.XtraEditors.DXErrorProvider;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace App_DB_3.Model
 {
-    public partial class Customers
+    public partial class Customers : IDXDataErrorInfo
     {
         [Key]
         public int customer_id { get; set; }
@@ -14,5 +16,60 @@ namespace App_DB_3.Model
         public string city { get; set; }
         public string state { get; set; }
         public string zip_code { get; set; }
+
+        public void GetError(ErrorInfo info)
+        {
+            //info.ErrorType = ErrorType.Critical;
+            //info.ErrorText = "This object cannot be saved.";
+        }
+        public void GetPropertyError(string propertyName, ErrorInfo info)
+        {
+            Console.WriteLine("Property name: " + propertyName);
+            switch (propertyName)
+            {
+                case "first_name":
+                    if (string.IsNullOrEmpty(first_name))
+                    {
+                        info.ErrorText = "Jméno nesmí být prázdné";
+                        info.ErrorType = ErrorType.Critical;
+                    }
+                    break;
+                case "last_name":
+                    if (string.IsNullOrEmpty(last_name))
+                    {
+                        info.ErrorText = "Příjmení nesmí být prázdné";
+                        info.ErrorType = ErrorType.Critical;
+                    }
+                    break;
+                case "phone":
+                    if (string.IsNullOrEmpty(phone) || !phone.Contains("+"))
+                    {
+                        info.ErrorText = "Telefon musí obsahovat znaménko +";
+                        info.ErrorType = ErrorType.Critical;
+                    }
+                    break;
+                case "email":
+                    if (string.IsNullOrEmpty(email))
+                    {
+                        info.ErrorText = "Email nemsmý být prázdný";
+                        info.ErrorType = ErrorType.Critical;
+                    }
+                    else if (!email.Contains("@") || !email.Contains("."))
+                    {
+                        info.ErrorText = "Neplatný formát.";
+                        info.ErrorType = ErrorType.Critical;
+                    }
+                    break;
+                case "state":
+                    if (string.IsNullOrEmpty(state))
+                    {
+                        info.ErrorText = "Stát nesmý být prázdný";
+                        info.ErrorType = ErrorType.Warning;
+                    }
+                    break;
+            }
+        }
     }
 }
+
+
